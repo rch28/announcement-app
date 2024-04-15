4750cd9b17be1d66084ea3f3b4faec2afd7ce136
iamxettry <rajuchhetry11@gmail.com>
2024-04-16 00:24:59 +0545

    modified

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/layout/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +11,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="">
+      <body className={`${inter.className} bg-[#E6FFFF] dark:bg-[#09010F]`}>
+        <Navbar/>
+        <div className="max-w-6xl mx-auto ">
+        {children}
+        </div>
+        
+        </body>
     </html>
   );
 }
