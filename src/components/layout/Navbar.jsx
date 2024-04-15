699735e3fd5d064b17d1920cb7c9dd9de5eaed63
iamxettry <rajuchhetry11@gmail.com>
2024-04-16 00:23:13 +0545

    Navbar added

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,80 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className=" border-b  border-gray-400/50  dark:border-gray-200/25 ">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+          <Link
+            href="/"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+          >
+           
+            <span className="text-3xl">👩🏻‍🎤</span>
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Announcement
+            </span>
+          </Link>
+          <div className="flex items-center space-x-6 rtl:space-x-reverse">
+            <Link
+              href="tel:5541251234"
+              className="text-sm  text-gray-500 dark:text-white hover:underline"
+            >
+              (555) 412-1234
+            </Link>
+            <Link
+              href="/register"
+              className="text-sm  text-blue-600 dark:text-blue-500 hover:underline"
+            >
+              Login
+            </Link>
+          </div>
+        </div>
+      </nav>
+      <nav className=" border-b border-gray-400/50 dark:border-gray-200/25 ">
+        <div className="max-w-screen-xl px-4 py-3 mx-auto">
+          <div className="flex items-center">
+            <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+              <li>
+                <Link
+                  href="#"
+                  className="text-gray-900 dark:text-white hover:underline"
+                  aria-current="page"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="text-gray-900 dark:text-white hover:underline"
+                >
+                  Company
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="text-gray-900 dark:text-white hover:underline"
+                >
+                  Team
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="text-gray-900 dark:text-white hover:underline"
+                >
+                  Features
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
