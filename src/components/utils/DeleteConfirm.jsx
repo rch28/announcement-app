57ec53a8aa6e02898fc54ec67526b77fa46efee1
iamxettry <rajuchhetry11@gmail.com>
2024-05-14 00:04:20 +0545

    fix: delete component

--- src/components/utils/DeleteConfirm.jsx
+++ src/components/utils/DeleteConfirm.jsx
@@ -2,15 +2,12 @@ import React from 'react'
 
 const DeleteConfirm = ({children, title }) => {
   return (
-    <div className="bg-white border-2 border-gray-300 shadow-md shadow-purple-200 p-4 rounded-lg w-88">
-    <div className="flex flex-col gap-4">
-      <h1 className="text-2xl font-bold">{title}</h1>
-      <p className="text-gray-500 text-sm">
-        Are you sure you want to delete?
-      </p>
-        {children}
-    </div>
+    <div className="bg-white border-2 border-gray-200 shadow-lg rounded-lg p-6 max-w-sm mx-auto">
+    <h1 className="text-3xl font-bold text-gray-900 mb-4">{title}</h1>
+    <p className="text-gray-600 text-sm mb-6">Are you sure you want to delete?</p>
+    {children}
   </div>
+  
 
   )
 }
