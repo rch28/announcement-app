3040f8d0b44657642fc9138f912558d0b994d349
iamxettry <rajuchhetry11@gmail.com>
2024-05-14 11:46:05 +0545

    fix(component): group card

--- src/components/group/GroupCard.jsx
+++ src/components/group/GroupCard.jsx
@@ -27,7 +27,7 @@ const GroupCard = ({ data, groupAdminInfo }) => {
               width={600}
               alt="Group Image"
             />
-            <div className="flex flex-col gap-4 justify-between border border-gray-300 p-4 rounded-xl">
+            <div className="flex flex-col gap-4 justify-between border border-gray-300 p-2 sm:p-4 rounded-xl">
               <div className="flex flex-col gap-2 max-w-96 ">
                 <div className="flex items-center gap-4">
                   <div className="bg-gray-100 rounded-full px-3 py-1 text-sm font-medium dark:bg-gray-800 text-gray-600 capitalize">
@@ -35,7 +35,7 @@ const GroupCard = ({ data, groupAdminInfo }) => {
                   </div>
                   <Rating rating={data?.average_rating} />
                 </div>
-                <h3 className="text-3xl font-bold">{data?.name}</h3>
+                <h3 className=" text-2xl xs:text-3xl font-bold">{data?.name}</h3>
                 <p className="text-sm text-gray-500 dark:text-gray-400 line-clamp-2">
                   {data?.description}
                 </p>
@@ -45,7 +45,7 @@ const GroupCard = ({ data, groupAdminInfo }) => {
                   <span>{data?.total_members} members</span>
                 </div>
               </div>
-              <div className="flex justify-between items-center pr-1 lg:pr-4">
+              <div className="flex justify-between items-center  lg:pr-4">
                 {groupAdminInfo !== undefined && (
                   <div className="flex items-center gap-2">
                     {groupAdminInfo?.image ? (
@@ -58,14 +58,15 @@ const GroupCard = ({ data, groupAdminInfo }) => {
                       <Avatar
                         name={`${groupAdminInfo?.first_name} ${groupAdminInfo?.last_name}`}
                         size="50"
+                        className="text-xs md:text-sm h-12 w-12"
                         round={true}
                         color="rgb(126 34 206 )"
                       />
                     )}
-                    <div className="text-sm">
+                    <div className="text-xs md:text-sm">
                       <p className="font-medium capitalize flex gap-2">
                         <span>{groupAdminInfo?.first_name}</span>
-                        <span>{groupAdminInfo?.last_name}</span>
+                        <span className="hidden md:flex">{groupAdminInfo?.last_name}</span>
                       </p>
                       <p className="text-gray-500 dark:text-gray-400">Admin</p>
                     </div>
@@ -74,7 +75,7 @@ const GroupCard = ({ data, groupAdminInfo }) => {
                 {isGrupAdmin && userAuthenticated ? (
                   <div>
                     <button
-                      className="px-4 py-2 bg-purple-600 rounded-full  text-white font-bold hover:bg-purple-700 text-xs md:text-sm   "
+                      className="px-2 sm:px-4 py-2 bg-purple-600 rounded-full  text-white font-bold hover:bg-purple-700 text-xs md:text-sm   "
                       onClick={() =>
                         setToggleCreateAnnouncement(!toggleCreateAnnouncement)
                       }
