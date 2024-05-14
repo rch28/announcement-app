4c1dca6b445046cb717a599121198073eb50c5b4
iamxettry <rajuchhetry11@gmail.com>
2024-05-14 09:46:53 +0545

    fix(component): Rating

--- src/components/group/Rating.jsx
+++ src/components/group/Rating.jsx
@@ -7,14 +7,17 @@ import React, { useEffect, useRef, useState } from "react";
 import toast from "react-hot-toast";
 
 const Rating = ({ rating }) => {
-    const searchParams = useSearchParams();
-    const group_id = searchParams.get("group_id");
+  const searchParams = useSearchParams();
+  const group_id = searchParams.get("group_id");
   const toggleRating = useStore((state) => state.toggleRating);
   const setToggleRating = useStore((state) => state.setToggleRating);
   const userData = useStore((state) => state.userData);
+  const userAuthenticated= useStore((state)=>state.userAuthenticated)
+
   const [giveRating, setGiveRating] = useState("");
+
   const inputRef = useRef(null);
-    const access_token = Cookies.get("access_token");
+  const access_token = Cookies.get("access_token");
   useEffect(() => {
     setGiveRating(rating);
     if (inputRef.current !== null) {
@@ -24,31 +27,43 @@ const Rating = ({ rating }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://127.0.0.1:8000/api/v1/group/give/rating/",{
-        method:"POST",
-        headers:{
-            "Content-Type":"application/json",
-            Authorization: `Bearer ${access_token}`,
+    const response = await fetch(
+      "http://127.0.0.1:8000/api/v1/group/give/rating/",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${access_token}`,
         },
-        body:JSON.stringify({rating:giveRating,group:group_id,user:userData?.id})
-            
-    })
-    if(response.ok){
-        const result = await response.json();
-        setToggleRating(false)
-        toast.success(result.msg)
+        body: JSON.stringify({
+          rating: giveRating,
+          group: group_id,
+          user: userData?.id,
+        }),
+      }
+    );
+    if (response.ok) {
+      const result = await response.json();
+      setToggleRating(false);
+      toast.success(result.msg);
+    } else {
+      const result = await response.json();
+      toast.error(result.errors[0].detail);
     }
-    else{
-        const result = await response.json();
-        toast.error(result.errors[0].detail)
+  };
+  const handleClick = () => {
+    if(!userAuthenticated){
+      toast.error("Not logged In!!")
+      return;
     }
+    setToggleRating(!toggleRating)
   };
   return (
     <div className="relative">
       <div
         title="rating"
         className="flex items-center gap-1 text-sm text-gray-500 cursor-pointer dark:text-gray-400"
-        onClick={() => setToggleRating(!toggleRating)}
+        onClick={handleClick}
       >
         <Star className="w-4 h-4 text-gray-500 fill-black dark:text-gray-400" />
         <span>{rating}</span>
@@ -58,8 +73,10 @@ const Rating = ({ rating }) => {
         <div className="flex items-center gap-1 w-52 rounded-md bg-white border border-gray-300 shadow-md shadow-gray-500 text-sm text-gray-500 dark:text-gray-400 absolute top-0 ">
           <div className="p-4">
             <p className="flex justify-end items-center">
-
-            <XIcon className="w-4 h-4 text-gray-700 fill-black dark:text-gray-400 cursor-pointer" onClick={() => setToggleRating(!toggleRating)} />
+              <XIcon
+                className="w-4 h-4 text-gray-700 fill-black dark:text-gray-400 cursor-pointer"
+                onClick={() => setToggleRating(!toggleRating)}
+              />
             </p>
             <div className="text-2xl text-gray-800 font-bold tracking-tighter pb-3">
               Rate Us
