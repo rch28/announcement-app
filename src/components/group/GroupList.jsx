0742a8ecaaa1882e813767d79d9815c8e2dca736
iamxettry <rajuchhetry11@gmail.com>
2024-05-15 09:01:24 +0545

    fix:group long description

--- src/components/group/GroupList.jsx
+++ src/components/group/GroupList.jsx
@@ -104,7 +104,7 @@ const GroupList = () => {
                     />
                     <div className="grid gap-2">
                       <h3 className="text-xl font-bold">{data?.name}</h3>
-                      <p className="text-sm text-gray-500 dark:text-gray-400">
+                      <p className="text-sm text-gray-500 dark:text-gray-400 line-clamp-4">
                         {data?.description}
                       </p>
                       <div className="flex items-center gap-2 text-sm">
