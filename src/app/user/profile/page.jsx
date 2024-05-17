1bd065993813a31eda0ca698e12ca64041626591
iamxettry <rajuchhetry11@gmail.com>
2024-05-17 23:39:15 +0545

    fix : style change

--- src/app/user/profile/page.jsx
+++ src/app/user/profile/page.jsx
@@ -1,47 +1,67 @@
-import EditProfile from "@/components/profile/EditProfile";
-import EditProfileImg from "@/components/profile/EditProfileImg";
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { profile } from "../../../../public";
 import Image from "next/image";
 import Link from "next/link";
-import { Plus } from "lucide-react";
+import { Plus, XIcon } from "lucide-react";
 import UserName from "@/components/utils/UserName";
-
+import { useStore } from "@/stores/store";
+import EditProfile from "@/components/profile/EditProfile";
+import PopUpWrapper from "@/components/PopUpWrapper";
 const ProfilePage = () => {
+  const [toggle, setToggle] = useState(false);
+  const userData = useStore((state) => state.userData);
   return (
     <div className="  min-h-screen flex  z-10">
       <section className="flex-[0.2]  py-4">
         <div className={""}>
           <Link
             href={"/user/profile/setting"}
-            className="cursor-pointer   bg-gray-200 text-center text-gray-500 text-sm  rounded-full h-20 w-20 relative"
+            className="cursor-pointer   bg-gray-200 text-center text-gray-500 text-sm  rounded-full h-40 w-40 relative"
           >
             <Image
-              src={profile}
-              height={200}
-              width={200}
+              className="rounded-full h-40 w-40 "
+              src={userData?.profilepic ? userData.profilepic : profile}
+              width={600}
+              height={600}
+              alt="profile"
               priority
-              alt="profile picture"
-              className="rounded-full"
             />
+
             <span className=" bg-green-500 text-white flex justify-center items-center w-12 h-12 rounded-full absolute right-1   bottom-1  cursor-pointer">
               <Plus size={24} />
             </span>
           </Link>
           <UserName />
           <div className="flex justify-center items-center w-full my-4 ">
-            <button className="mx-2 py-2  border border-purple-400 w-full text-gray-800 font-medium  rounded-md bg-white/40">
+            <button
+              className="mx-2 py-2  border border-purple-400 w-full text-gray-800 font-medium  rounded-md bg-white/40 cursor-pointer"
+              onClick={() => setToggle(!toggle)}
+            >
               Edit profile
             </button>
           </div>
         </div>
       </section>
-      <section className="p-4 bg-amber-400 w-full flex-1">
-        content
-      </section>
-      {/* <EditProfileImg />
-
-        <EditProfile/> */}
+      {toggle && (
+        <PopUpWrapper>
+          <div className="p-8 w-[45%] bg-white rounded-xl shadow-xl shadow-gray-500 relative">
+            <button
+              className="absolute right-2 top-2 text-red-600 hover:bg-red-200 bg-white shadow-md shadow-gray-500 rounded-full p-1 "
+              onClick={() => setToggle(!toggle)}
+            >
+              {" "}
+              <XIcon />{" "}
+            </button>
+            <h1 className="border-b border-gray-400 py-2 w-full text-xl tracking-tighter font-medium text-black/80 my-4">
+              Update Your Info
+            </h1>
+            <EditProfile />
+          </div>
+        </PopUpWrapper>
+      )}
+      <section className="p-4  w-full flex-1">content</section>
+    
     </div>
   );
 };
--- src/app/user/profile/setting/page.jsx
+++ src/app/user/profile/setting/page.jsx
@@ -4,11 +4,11 @@ import React from 'react'
 
 const Setting = () => {
   return (
-    <div className='p-4 w-full bg-white rounded-xl shadow-xl shadow-gray-500'>
+    <div className='p-8 pt-4 w-full bg-white rounded-xl shadow-xl shadow-gray-500'>
       <h1 className='border-b border-gray-400 py-2 w-full text-xl tracking-tighter font-medium text-black/80 '>Manage Profile</h1>
 
       <div className="flex gap-4 md:gap-2 mt-4">
-        <div className='flex-1'>
+        <div className='flex-1 md:pr-16'>
           <EditProfile/>
         </div>
         <div className='flex-[0.3]'>
