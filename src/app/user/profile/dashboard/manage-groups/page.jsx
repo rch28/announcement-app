74ba90ea11ac61c3f330e2e8f0793f151067f0c1
iamxettry <rajuchhetry11@gmail.com>
2024-05-19 01:08:33 +0545

    fix : group list

--- src/app/user/profile/dashboard/manage-groups/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ManageGroups = () => {
-  return (
-    <div>ManageGroups</div>
-  )
-}
-
-export default ManageGroups
--- src/app/user/profile/dashboard/page.jsx
+++ src/app/user/profile/dashboard/page.jsx
@@ -1,11 +1,88 @@
-import React from 'react'
+"use client";
+import PopUpWrapper from "@/components/PopUpWrapper";
+import GroupCard from "@/components/profile/dashboard/GroupCard";
+import DeleteConfirm from "@/components/utils/DeleteConfirm";
+import { fetchAllData } from "@/index";
+import React, { useEffect, useState } from "react";
 
 const Dashboard = () => {
+  const [switchGroup, setSwitchGroup] = useState(true);
+  const [data, setData] = useState(null);
+  const [deleteToggle, setDeleteToggle] = useState(false);
+  useEffect(() => {
+    const fetchGroup = async () => {
+      const allData = await fetchAllData(
+        "http://127.0.0.1:8000/api/v1/group/created-by/user/"
+      );
+      setData(allData);
+    };
+    fetchGroup();
+  }, []);
+  const handleDeleteGroup = ()=>{
+    console.log("delete");
+  }
   return (
     <div>
-     Dashboard
-    </div>
-  )
-}
+      <nav className="flex justify-between items-center bg-gray-300 shadow-xl rounded-xl ">
+        <button
+          onClick={() => setSwitchGroup(true)}
+          className={`bg-white  p-3 text-center cursor-pointer transition-all ease-linear duration-100  ${
+            switchGroup
+              ? "flex-1 rounded-xl"
+              : "md:flex-[.4] bg-gray-300 rounded-l-xl"
+          }`}
+        >
+          <h1 className="font-medium text-sm">My Groups</h1>
+        </button>
+        <button
+          onClick={() => setSwitchGroup(false)}
+          className={`  p-3 text-center cursor-pointer  transition-all ease-linear duration-100  ${
+            switchGroup
+              ? "md:flex-[.4] bg-gray-300  rounded-r-xl"
+              : "flex-1 bg-white  rounded-xl "
+          }`}
+        >
+          <h1 className="font-medium text-sm">Joined Groups</h1>
+        </button>
+      </nav>
+      <div className="flex-1  min-h-80 rounded-xl mt-4 ">
+        {switchGroup ? (
+          <div className="  grid md:grid-cols-2 gap-6 ">
+            {data?.map((group) => (
+              <GroupCard
+                group={group}
+                key={group.group_id}
+                setDeleteToggle={setDeleteToggle}
+              />
+            ))}
+          </div>
+        ) : (
+          <p>Joined</p>
+        )}
+      </div>
 
-export default Dashboard
+      {deleteToggle && (
+        <PopUpWrapper>
+          <DeleteConfirm title={"Delete Group"}>
+            <div className="flex gap-4 ">
+              <button
+                onClick={() => setDeleteToggle(!deleteToggle)}
+                className="px-6 py-2 bg-gray-600 rounded-full  text-white font-bold hover:bg-gray-700"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={handleDeleteGroup}
+                className="px-6 py-2 bg-red-600 rounded-full  text-white font-bold hover:bg-red-700"
+              >
+                Delete
+              </button>
+            </div>
+          </DeleteConfirm>
+        </PopUpWrapper>
+      )}
+    </div>
+  );
+};
+
+export default Dashboard;
