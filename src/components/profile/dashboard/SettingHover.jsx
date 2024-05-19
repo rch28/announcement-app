18d6b8a03c368260510ca0d175e1eda55862c750
iamxettry <rajuchhetry11@gmail.com>
2024-05-19 13:27:57 +0545

    feat : view announcements

--- src/components/profile/dashboard/SettingHover.jsx
+++ src/components/profile/dashboard/SettingHover.jsx
@@ -1,7 +1,8 @@
 "use client";
 import CardUtil from "@/components/utils/CardUtil";
 import { useStore } from "@/stores/store";
-import { NotebookTabs, UserPlus } from "lucide-react";
+import { NotebookPenIcon, NotebookTextIcon, UserPlus } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 const SettingHover = ({ group,SetGroupData }) => {
@@ -21,10 +22,20 @@ const SettingHover = ({ group,SetGroupData }) => {
         <CardUtil
           title="Create Announcement"
           icon={
-            <NotebookTabs className="h-4 w-4 text-purple-700 dark:text-gray-400" />
+            <NotebookPenIcon className="h-4 w-4 text-purple-700 dark:text-gray-400" />
           }
         />
       </button>
+      <Link href={`/announcements/${group.name}/announcements-list?group_id=${group.group_id}`}
+        className="cursor-pointer hover:bg-gray-300 p-2 rounded-md"
+      >
+        <CardUtil
+          title="View Announcements"
+          icon={
+            <NotebookTextIcon className="h-4 w-4 text-purple-700 dark:text-gray-400" />
+          }
+        />
+      </Link>
       <div className="cursor-pointer hover:bg-gray-300 p-2 rounded-md">
         <CardUtil
           title="Manage Members"
