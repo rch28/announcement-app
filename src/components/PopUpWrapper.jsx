151e73789626a7f11fdb4cfa3ab6b4a77bff8250
iamxettry <rajuchhetry11@gmail.com>
2024-05-20 08:54:59 +0545

    fix : backdrop filter

--- src/components/PopUpWrapper.jsx
+++ src/components/PopUpWrapper.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PopUpWrapper = ({children}) => {
   return (
-    <div className='fixed top-0 left-0 flex w-screen h-screen justify-center items-center bg-black/50'>{children}</div>
+    <div className='fixed top-0 left-0 z-50 flex w-screen h-screen justify-center items-center backdrop-blur-sm bg-black/50'>{children}</div>
   )
 }
 
