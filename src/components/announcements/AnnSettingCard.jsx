4871f5c88804e0dd8125a12491085d512fed66c6
iamxettry <rajuchhetry11@gmail.com>
2024-05-21 20:36:25 +0545

    fix:  delete confirmation

--- src/components/announcements/AnnSettingCard.jsx
+++ src/components/announcements/AnnSettingCard.jsx
@@ -1,62 +1,70 @@
-import React, { useState } from 'react'
-import CardUtil from '../utils/CardUtil'
-import { Delete, Edit, UserPlus, Users, XIcon} from 'lucide-react'
-import DeleteConfirm from '../utils/DeleteConfirm'
-import { useRouter, useSearchParams } from 'next/navigation'
-import Cookies from 'js-cookie'
-import { useStore } from '@/stores/store'
-import toast from 'react-hot-toast'
+import React, { useState } from "react";
+import CardUtil from "../utils/CardUtil";
+import { Delete, Edit, UserPlus, Users, XIcon } from "lucide-react";
+import DeleteConfirm from "../utils/DeleteConfirm";
+import { useRouter, useSearchParams } from "next/navigation";
+import Cookies from "js-cookie";
+import { useStore } from "@/stores/store";
+import toast from "react-hot-toast";
+import PopUpWrapper from "../PopUpWrapper";
 
-const AnnSettingCard = ({setToggle ,setToggleEdit}) => {
-    const router = useRouter();
-    const searchParams = useSearchParams();
-    const [deleteToggle, setDeleteToggle] = useState(false);
-    const ann_id = searchParams.get("ann_id");
-    const access_token = Cookies.get("access_token");
-    const setToggleCreateAnnouncement = useStore((state) => state.setToggleCreateAnnouncement);
-    const handleDeleteGroup = () => {
-      const newPromise = new Promise(async (resolve, reject) => {
-        const response = await fetch(
-          `http://127.0.0.1:8000/api/v1/announcement/delete/${ann_id}/`,
-          {
-            method: "DELETE",
-            headers: {
-              authorization: `Bearer ${access_token}`,
-            },
-          }
-        );
-        if (response.ok) {
-          setToggle(false);
-          router.push("/groups");
-          resolve();
-        } else {
-          const result = await response.json();
-          setToggle(false);
-          reject(result);
+const AnnSettingCard = ({ setToggle, setToggleEdit }) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [deleteToggle, setDeleteToggle] = useState(false);
+  const ann_id = searchParams.get("ann_id");
+  const access_token = Cookies.get("access_token");
+  const setToggleCreateAnnouncement = useStore(
+    (state) => state.setToggleCreateAnnouncement
+  );
+  const handleDeleteGroup = () => {
+    const newPromise = new Promise(async (resolve, reject) => {
+      const response = await fetch(
+        `http://127.0.0.1:8000/api/v1/announcement/delete/${ann_id}/`,
+        {
+          method: "DELETE",
+          headers: {
+            authorization: `Bearer ${access_token}`,
+          },
         }
-      });
-      toast.promise(newPromise, {
-        loading: "Deleting announcement...",
-        success: "Announcement deleted successfully!",
-        error: (data) => data.errors[0].detail || "Failed to delete announcement",
-      });
-    };
-    const handleEditAnn = () => {
-        setToggleEdit(true)
-        setToggleCreateAnnouncement(true)
-
-    }
+      );
+      if (response.ok) {
+        setToggle(false);
+        router.push("/groups");
+        resolve();
+      } else {
+        const result = await response.json();
+        setToggle(false);
+        reject(result);
+      }
+    });
+    toast.promise(newPromise, {
+      loading: "Deleting announcement...",
+      success: "Announcement deleted successfully!",
+      error: (data) => data.errors[0].detail || "Failed to delete announcement",
+    });
+  };
+  const handleEditAnn = () => {
+    setToggleEdit(true);
+    setToggleCreateAnnouncement(true);
+  };
   return (
     <div className="flex flex-col p-2 pt-0 gap-6 relative">
       <div>
-        <div className='flex justify-end absolute right-0 -top-2'>
-            <button  className="p-1 flex justify-center items-center dark:text-gray-400 cursor-pointer text-red-500 bg-white shadow-sm shadow-gray-900 rounded-full hover:bg-red-200" onClick={()=>setToggle(false)}>
-
-            <XIcon size={24}/>
-            </button>
+        <div className="flex justify-end absolute right-0 -top-2">
+          <button
+            className="p-1 flex justify-center items-center dark:text-gray-400 cursor-pointer text-red-500 bg-white shadow-sm shadow-gray-900 rounded-full hover:bg-red-200"
+            onClick={() => setToggle(false)}
+          >
+            <XIcon size={24} />
+          </button>
         </div>
-        <h1 className="text-3xl font-bold tracking-tighter">Announcement Setting</h1>
-        <p className="text-gray-500 ">Manage your Announcement settings and members</p>
+        <h1 className="text-3xl font-bold tracking-tighter">
+          Announcement Setting
+        </h1>
+        <p className="text-gray-500 ">
+          Manage your Announcement settings and members
+        </p>
       </div>
       <div className="flex flex-col gap-2 border-b pb-4">
         <div className="cursor-pointer hover:bg-gray-300 p-2 rounded-md">
@@ -102,11 +110,7 @@ const AnnSettingCard = ({setToggle ,setToggleEdit}) => {
         </div>
       </div>
       {deleteToggle && (
-        <div
-          className={` fixed top-0 left-0 flex w-screen h-screen justify-center items-center ${
-            deleteToggle && "bg-black/30 "
-          }`}
-        >
+        <PopUpWrapper>
           <DeleteConfirm title={"Delete announcement"}>
             <div className="flex gap-4 ">
               <button
@@ -121,12 +125,12 @@ const AnnSettingCard = ({setToggle ,setToggleEdit}) => {
               >
                 Delete
               </button>
-            </div>  
+            </div>
           </DeleteConfirm>
-        </div>
+        </PopUpWrapper>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default AnnSettingCard
+export default AnnSettingCard;
