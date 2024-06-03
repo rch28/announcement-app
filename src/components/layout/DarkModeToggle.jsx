73e5ae8a69d46924f3e6b09cefabf0ffd86de1b7
iamxettry <rajuchhetry11@gmail.com>
2024-06-03 12:38:04 +0545

    feat : Darkmode added

---
+++ src/components/layout/DarkModeToggle.jsx
@@ -0,0 +1,41 @@
+"use client";
+import { Moon, Sun } from "lucide-react";
+import { useEffect, useState } from "react";
+
+const DarkModeToggle = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    if (
+      theme === "dark" ||
+      (!theme && window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      setIsDarkMode(true);
+    } else {
+      setIsDarkMode(false);
+    }
+  }, []);
+
+  const toggleDarkMode = () => {
+    if (isDarkMode) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    }
+    setIsDarkMode(!isDarkMode);
+  };
+
+  return (
+    <button
+      onClick={toggleDarkMode}
+      className="p-2 rounded-full   dark:shadow-sm text-xl shadow-md  w-8 h-8 flex justify-center items-center shadow-gray-500 cursor-pointer"
+    >
+      {isDarkMode ? <Sun /> : <Moon />}
+    </button>
+  );
+};
+
+export default DarkModeToggle;
