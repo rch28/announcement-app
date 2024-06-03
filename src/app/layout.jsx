3a4aad4e619904ff1587b242455b5b83ff943768
iamxettry <rajuchhetry11@gmail.com>
2024-06-03 12:40:45 +0545

    feat : darkmode refresh problem fix

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,8 @@
+import { Toaster } from "react-hot-toast";
 import "./globals.css";
 
-import Main from "./Main";
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
 
 
 export const metadata = {
@@ -11,9 +13,32 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <>
-    <Main>
-      {children}
-    </Main>
+    <html lang="en">
+      <body className={` bg-gray-200 dark:bg-[#000000] dark:text-white  `}>
+      <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                function setTheme(theme) {
+                  document.documentElement.className = theme;
+                  localStorage.setItem('theme', theme);
+                }
+                const theme = localStorage.getItem('theme');
+                if (theme === 'dark' || (!theme && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                  setTheme('dark');
+                } else {
+                  setTheme('light');
+                }
+              })();
+            `,
+          }}
+        />
+        <Toaster />
+        <Navbar />
+        <div className="max-w-5xl mx-auto ">{children}</div>
+        <Footer />
+      </body>
+    </html>
     </>
   );
 }
