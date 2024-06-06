5781c3b35991cd9a09cc81eb8594b1b0e45bc932
iamxettry <rajuchhetry11@gmail.com>
2024-06-06 11:29:46 +0545

    feat : Group type added

--- src/components/group/CreateGroup.jsx
+++ src/components/group/CreateGroup.jsx
@@ -15,6 +15,7 @@ const CreateGroup = ({ mode, data }) => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [category, setCategory] = useState("");
+  const [groupType, setGroupType] = useState("")
   const [image, setImage] = useState("");
   const access_token = Cookies.get("access_token");
   const setToggleCreateGroup = useStore((state) => state.setToggleCreateGroup);
@@ -36,6 +37,7 @@ const CreateGroup = ({ mode, data }) => {
   }, [mode]);
   const handleSubmit = async (e) => {
     e.preventDefault();
+    console.log(groupType);
     if (!name) {
       toast.error("Group name is required");
       return;
@@ -48,6 +50,10 @@ const CreateGroup = ({ mode, data }) => {
       toast.error("Please select a category");
       return;
     }
+    if(groupType === ""){
+      toast.error("Please select a group type")
+      return;
+    }
     if (!image && mode !== "edit") {
       toast.error("Group image is required");
       return;
@@ -57,6 +63,7 @@ const CreateGroup = ({ mode, data }) => {
     data.append("name", name);
     data.append("description", description);
     data.append("category", category);
+    data.append("group_type", groupType)
     const newPromise = new Promise(async (resolve, reject) => {
       const response = await fetch(
         `http://127.0.0.1:8000/api/v1/group/${
@@ -122,6 +129,11 @@ const CreateGroup = ({ mode, data }) => {
     { value: "travel", label: "Travel" },
     { value: "other", label: "Other" },
   ];
+  const group_type= [
+    {value: "", label: "Select Type"},
+    { value: "public", label: "Public" },
+    { value: "private", label: "Private" },
+  ]
   return (
     <div className="bg-white mx-4  rounded-xl border-2  w-[90%] md:w-auto shadow-lg shadow-gray-500 relative z-20 p-4 dark:shadow-none">
       <div className="flex justify-end items-center  absolute right-4 top-2 ">
@@ -133,10 +145,10 @@ const CreateGroup = ({ mode, data }) => {
         </button>
       </div>
       <div className="p-4">
-        <h1 className=" text-2xl  font-bold tracking-widest text-black">
+        <h1 className="text-lg md:text-2xl  font-bold tracking-widest text-black">
           {mode === "edit" ? "Edit Your group" : "Create New Group"}
         </h1>
-        <CardDescription className="text-sm text-gray-900">
+        <CardDescription className="text-sm text-gray-900 dark:text-gray-700">
           {mode === "edit" ? "Edit your Group." : "Create a new group."}
         </CardDescription>
       </div>
@@ -147,10 +159,10 @@ const CreateGroup = ({ mode, data }) => {
           onSubmit={handleSubmit}
           encType="multipart/form-data"
         >
-          <div className="grid md:grid-cols-2 md:gap-4">
+          <div className="grid gap-4 md:grid-cols-2 ">
             <div className="grid gap-4">
               <div className=" md:space-y-2">
-                <Label htmlFor="group_name">Group name</Label>
+                <Label className="hidden md:flex " htmlFor="group_name">Group name</Label>
                 <Input
                   type="text"
                   name="group_name"
@@ -161,8 +173,8 @@ const CreateGroup = ({ mode, data }) => {
                   className="border border-gray-600 focus:border-purple-500 bg-white"
                 />
               </div>
-              <div className="  md:space-y-2">
-                <Label htmlFor="group_description">Description</Label>
+              <div className="">
+                <Label className="hidden md:flex " htmlFor="group_description">Description</Label>
                 <Textarea
                   type="text"
                   name="group_description"
@@ -176,7 +188,7 @@ const CreateGroup = ({ mode, data }) => {
             </div>
             <div className="flex flex-col gap-4">
               <div className=" group md:space-y-2">
-                <Label htmlFor="category">Select Category</Label>
+                <Label className="hidden md:flex " htmlFor="category">Select Category</Label>
 
                 <select
                   name="category"
@@ -195,8 +207,28 @@ const CreateGroup = ({ mode, data }) => {
                   ))}
                 </select>
               </div>
+              <div className=" group md:space-y-2">
+                <Label className="hidden md:flex " htmlFor="groupType">Select Type</Label>
+
+                <select
+                  name="groupType"
+                  value={groupType}
+                  onChange={(e) => setGroupType(e.target.value)}
+                  className="block w-full px-4 py-2 text-gray-500 bg-white border border-gray-600  focus:border-purple-500 rounded-md focus:outline-none text-sm font-medium appearance-none "
+                >
+                  {group_type.map((type) => (
+                    <option
+                      className="w-fit py-2 px-4 text-black"
+                      key={type.value}
+                      value={type.value}
+                    >
+                      {type.label}
+                    </option>
+                  ))}
+                </select>
+              </div>
               <div className="">
-                <Label htmlFor="group_image ">Image</Label>
+                <Label className="hidden md:flex " htmlFor="group_image ">Image</Label>
                 <input
                   type="file"
                   name="group_image"
@@ -212,7 +244,7 @@ const CreateGroup = ({ mode, data }) => {
               </div>
             </div>
           </div>
-          <div>
+          <div className="mt-4">
             <div className="flex justify-end">
               <button
                 type="submit"
