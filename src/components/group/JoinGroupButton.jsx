7db97a771962f90139d299b09b9e4234328d6a70
iamxettry <rajuchhetry11@gmail.com>
2024-06-07 17:07:24 +0545

    fix(parameter) : parameter name

--- src/components/group/JoinGroupButton.jsx
+++ src/components/group/JoinGroupButton.jsx
@@ -92,7 +92,7 @@ const JoinGroupButton = () => {
             Authorization: `Bearer ${access_token}`,
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({ group_id: groupId }),
+          body: JSON.stringify({ group: groupId }),
         }
       );
 
