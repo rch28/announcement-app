ddcd887f2aff583fadb7a2b33de6a8ba7d2166ee
iamxettry <rajuchhetry11@gmail.com>
2024-06-07 10:27:51 +0545

    fix : main bg color

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <>
     <html lang="en">
-      <body className={` bg-gray-200 dark:bg-[#000000] dark:text-white  `}>
+      <body className={` bg-primary  dark:bg-dark-primary dark:text-white  `}>
       <script
           dangerouslySetInnerHTML={{
             __html: `
