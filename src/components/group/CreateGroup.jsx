ce19a4f307a26a208edb866e9eee266395008106
iamxettry <rajuchhetry11@gmail.com>
2024-07-10 20:10:46 +0545

    feat : Custome Category

--- src/components/group/CreateGroup.jsx
+++ src/components/group/CreateGroup.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useStore } from "@/stores/store";
 import Cookies from "js-cookie";
-import { XIcon } from "lucide-react";
+import { XCircleIcon, XIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useRef, useState } from "react";
 import toast from "react-hot-toast";
@@ -13,22 +13,26 @@ import { Textarea } from "../ui/textarea";
 const CreateGroup = ({ mode, data }) => {
   const router = useRouter();
   // input refs
-  const groupNameRef = useRef(null)
-  const descRef=useRef(null)
-  const categoryRef=useRef(null)
-  const typeRef=useRef(null)
-  const imageRef=useRef(null)
+  const groupNameRef = useRef(null);
+  const descRef = useRef(null);
+  const categoryRef = useRef(null);
+  const CustomcategoryRef = useRef(null);
+  const typeRef = useRef(null);
+  const imageRef = useRef(null);
 
-
-
+  const [categoriesOptions, setCategoriesOptions] = useState([]);
+  const [createCategoryMode, setCreateCategoryMode] = useState(false);
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [category, setCategory] = useState("");
-  const [groupType, setGroupType] = useState("")
+  const [CustomeCategory, setCustomeCategory] = useState("");
+  const [groupType, setGroupType] = useState("");
   const [image, setImage] = useState("");
   const access_token = Cookies.get("access_token");
   const setToggleCreateGroup = useStore((state) => state.setToggleCreateGroup);
   const group_id = data?.group_id;
+  console.log(category);
+  console.log("custome", CustomeCategory);
 
   const setAnnouncementGroup = useStore((state) => state.setAnnouncementGroup);
   const handleFileChange = async (e) => {
@@ -44,40 +48,59 @@ const CreateGroup = ({ mode, data }) => {
       setCategory(data?.category);
     }
   }, [mode]);
+  console.log(createCategoryMode);
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(groupType);
     if (!name) {
-      groupNameRef.current.focus()
+      groupNameRef.current.focus();
       toast.error("Group name is required");
       return;
     }
     if (!description) {
-      descRef.current.focus()
+      descRef.current.focus();
       toast.error("Group description is required");
       return;
     }
-    if (category === "any" || category === "") {
-      categoryRef.current.focus()
-      toast.error("Please select a category");
-      return;
+    if (!createCategoryMode) {
+      if (category === "any" || category === "") {
+        categoryRef.current.focus();
+        toast.error("Please select a category");
+        return;
+      }
     }
-    if(groupType === ""){
-      typeRef.current.focus()
-      toast.error("Please select a group type")
+    if (createCategoryMode) {
+      if (!CustomeCategory) {
+        CustomcategoryRef.current.focus();
+        toast.error("Please enter a category");
+        return;
+      }
+    }
+    if (groupType === "") {
+      typeRef.current.focus();
+      toast.error("Please select a group type");
       return;
     }
     if (!image && mode !== "edit") {
-      imageRef.current.focus()
+      imageRef.current.focus();
       toast.error("Group image is required");
       return;
     }
+    let categoryId = null;
+    if (createCategoryMode && CustomeCategory) {
+      const res = await CategorySelectOrCreate();
+      categoryId = res.id;
+    }
+
     const data = new FormData();
     data.append("image", image);
     data.append("name", name);
     data.append("description", description);
-    data.append("category", category);
-    data.append("group_type", groupType)
+    if (createCategoryMode) {
+      data.append("category", categoryId);
+    } else {
+      data.append("category", category);
+    }
+    data.append("group_type", groupType);
     const newPromise = new Promise(async (resolve, reject) => {
       const response = await fetch(
         `${process.env.NEXT_PUBLIC_DB_BASE_URL}/group/${
@@ -94,7 +117,7 @@ const CreateGroup = ({ mode, data }) => {
       const result = await response.json();
       if (response.ok) {
         const res = await fetch(
-          "http://127.0.0.1:8000/api/v1/group/created-by/user/?limit=100",
+          `${process.env.NEXT_PUBLIC_DB_BASE_URL}/group/created-by/user/?limit=100`,
           {
             method: "GET",
             headers: {
@@ -127,8 +150,33 @@ const CreateGroup = ({ mode, data }) => {
     });
   };
 
+  const CategorySelectOrCreate = async () => {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_DB_BASE_URL}/group/category/create/`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name: CustomeCategory }),
+      }
+    );
+    if (response.ok) {
+      const result = await response.json();
+      console.log(result);
+      setCategory(result.id);
+      setCreateCategoryMode(false);
+      return result;
+    }
+    if (!response.ok) {
+      const result = await response.json();
+      console.log(result);
+    }
+  };
+
   const options = [
-    { value: "any", label: "Any Category" },
+    { value: "any", label: "Select Category" },
 
     { value: "web", label: "Web" },
     { value: "network", label: "Network" },
@@ -141,13 +189,38 @@ const CreateGroup = ({ mode, data }) => {
     { value: "lifestyle", label: "Lifestyle" },
     { value: "sports", label: "Sports" },
     { value: "travel", label: "Travel" },
-    { value: "other", label: "Other" },
   ];
-  const group_type= [
-    {value: "", label: "Select Type"},
+  const group_type = [
+    { value: "", label: "Select Type" },
     { value: "public", label: "Public" },
     { value: "private", label: "Private" },
-  ]
+  ];
+  useEffect(() => {
+    const fetchCategories = async () => {
+      if (category === "custom") {
+        CustomcategoryRef;
+        setCategory("");
+        setCreateCategoryMode(true);
+      }
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_DB_BASE_URL}/group/category/list/`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        }
+      );
+      if (response.ok) {
+        const result = await response.json();
+        if (result.count === 0) {
+          setCreateCategoryMode(true);
+        }
+        setCategoriesOptions(result);
+      }
+    };
+    fetchCategories();
+  }, [createCategoryMode, category]);
   return (
     <div className="bg-white mx-4  rounded-xl border-2  w-[90%] md:w-auto shadow-lg shadow-gray-500 relative z-20 p-4 dark:shadow-none">
       <div className="flex justify-end items-center  absolute right-4 top-2 ">
@@ -176,7 +249,9 @@ const CreateGroup = ({ mode, data }) => {
           <div className="grid gap-4 md:grid-cols-2 ">
             <div className="grid gap-4">
               <div className=" md:space-y-2">
-                <Label className="hidden md:flex " htmlFor="group_name">Group name</Label>
+                <Label className="hidden md:flex " htmlFor="group_name">
+                  Group name
+                </Label>
                 <Input
                   ref={groupNameRef}
                   type="text"
@@ -189,7 +264,9 @@ const CreateGroup = ({ mode, data }) => {
                 />
               </div>
               <div className="">
-                <Label className="hidden md:flex " htmlFor="group_description">Description</Label>
+                <Label className="hidden md:flex " htmlFor="group_description">
+                  Description
+                </Label>
                 <Textarea
                   ref={descRef}
                   type="text"
@@ -203,29 +280,73 @@ const CreateGroup = ({ mode, data }) => {
               </div>
             </div>
             <div className="flex flex-col gap-4">
-              <div className=" group md:space-y-2">
-                <Label className="hidden md:flex " htmlFor="category">Select Category</Label>
+              <div className=" group md:space-y-2 relative">
+                {createCategoryMode ? (
+                  <>
+                    <Label className="hidden md:flex " htmlFor="CustomCategory">
+                      Enter Category
+                    </Label>
+                    <Input
+                      ref={CustomcategoryRef}
+                      type="text"
+                      name="CustomCategory"
+                      id="CustomCategory"
+                      value={CustomeCategory}
+                      onChange={(e) => setCustomeCategory(e.target.value)}
+                      placeholder="Enter New Category"
+                      className="border border-gray-600 focus:border-purple-700 bg-white"
+                    />
+                    {categoriesOptions?.count !== 0 && (
+                      <span
+                        className="text-red-500 cursor-pointer absolute right-2 top-5"
+                        onClick={() => setCreateCategoryMode(false)}
+                      >
+                        <XCircleIcon />
+                      </span>
+                    )}
+                  </>
+                ) : (
+                  <>
+                    <Label className="hidden md:flex " htmlFor="category">
+                      Select Category
+                    </Label>
 
-                <select
-                  ref={categoryRef}
-                  name="category"
-                  value={category}
-                  onChange={(e) => setCategory(e.target.value)}
-                  className="block w-full px-4 py-2 text-gray-500 bg-white border border-gray-600  focus:border-purple-700 rounded-md focus:outline-none text-sm font-medium appearance-none "
-                >
-                  {options.map((option) => (
-                    <option
-                      className="w-fit py-2 px-4 text-black"
-                      key={option.value}
-                      value={option.value}
+                    <select
+                      ref={categoryRef}
+                      name="category"
+                      value={category}
+                      onChange={(e) => setCategory(e.target.value)}
+                      className="block w-full px-4 py-2 text-gray-500 bg-white border border-gray-600  focus:border-purple-700 rounded-md focus:outline-none text-sm font-medium appearance-none "
                     >
-                      {option.label}
-                    </option>
-                  ))}
-                </select>
+                      <option
+                        className="w-fit py-2 px-4 text-black"
+                        value={"any"}
+                      >
+                        Select Category
+                      </option>
+                      <option
+                        className="w-fit py-2 px-4 text-black"
+                        value={"custom"}
+                      >
+                        Custom Option
+                      </option>
+                      {categoriesOptions?.results?.map((option) => (
+                        <option
+                          className="w-fit py-2 px-4 text-black"
+                          key={option.id}
+                          value={option.id}
+                        >
+                          {option.name}
+                        </option>
+                      ))}
+                    </select>
+                  </>
+                )}
               </div>
               <div className=" group md:space-y-2">
-                <Label className="hidden md:flex " htmlFor="groupType">Select Type</Label>
+                <Label className="hidden md:flex " htmlFor="groupType">
+                  Select Type
+                </Label>
 
                 <select
                   ref={typeRef}
@@ -246,7 +367,9 @@ const CreateGroup = ({ mode, data }) => {
                 </select>
               </div>
               <div className="">
-                <Label className="hidden md:flex " htmlFor="group_image ">Image</Label>
+                <Label className="hidden md:flex " htmlFor="group_image ">
+                  Image
+                </Label>
                 <input
                   ref={imageRef}
                   type="file"
