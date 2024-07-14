25ab96ea16e8e8149fef7e9e77efec475c7b92ab
iamxettry <rajuchhetry11@gmail.com>
2024-07-14 09:53:38 +0545

    feat(page): create-new announcement page

---
+++ src/app/announcements/create-new/page.jsx
@@ -0,0 +1,12 @@
+import { AnnouncementCardForm } from '@/components/announcements/AnnouncementCardForm'
+import React from 'react'
+
+const CreateNewAnnouncement = () => {
+  return (
+    <div className=''>
+        <AnnouncementCardForm/>
+    </div>
+  )
+}
+
+export default CreateNewAnnouncement
