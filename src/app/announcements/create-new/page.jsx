e30d69315679ef99b602bf0123f245392b90e418
iamxettry <rajuchhetry11@gmail.com>
2024-07-14 16:55:29 +0545

    feat : group description

--- src/app/announcements/create-new/page.jsx
+++ src/app/announcements/create-new/page.jsx
@@ -1,12 +1,35 @@
-import { AnnouncementCardForm } from '@/components/announcements/AnnouncementCardForm'
-import React from 'react'
+"use client";
+import { AnnouncementCardForm } from "@/components/announcements/AnnouncementCardForm";
+import { useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 const CreateNewAnnouncement = () => {
-  return (
-    <div className=''>
-        <AnnouncementCardForm/>
-    </div>
-  )
-}
+  const searchParams = useSearchParams();
+  const group_id = searchParams.get("group_id");
+  const [groupData, setGroupData] = useState({})
+  useEffect(() => {
+    if (group_id) {
+      const fetchGroup = async () => {
+        try {
+          const response = await fetch(
+            `${process.env.NEXT_PUBLIC_DB_BASE_URL}/group/retrieve/${group_id}/`
+          );
+          if (!response.ok) return;
+          const result = await response.json();
+          setGroupData(result);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      fetchGroup();
+    }
+  }, [group_id]);
 
-export default CreateNewAnnouncement
+  return (
+    <div className="">
+      <AnnouncementCardForm group_id={group_id} group_name={groupData.name} />
+    </div>
+  );
+};
+
+export default CreateNewAnnouncement;
