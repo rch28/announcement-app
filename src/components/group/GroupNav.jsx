07cf7372cd697e465d33dc22af2a88a6fe5c9460
iamxettry <rajuchhetry11@gmail.com>
2024-07-19 08:46:47 +0545

    fix : role

--- src/components/group/GroupNav.jsx
+++ src/components/group/GroupNav.jsx
@@ -16,15 +16,11 @@ const GroupNav = () => {
   const groupAdmin = useStore((state) => state.groupAdmin);
   const isGrupAdmin = useStore((state) => state.isGrupAdmin);
   const setIsGroupAdmin = useStore((state) => state.setIsGroupAdmin);
-  const [toggleSetting, setToggleSetting] = useState(false)
-  const userAuthenticated= useStore(state=>state.userAuthenticated)
-  const joined = useStore(state=>state.Joined)
-  const toggleCreateAnnouncement = useStore(
-    (state) => state.toggleCreateAnnouncement
-  );
-  const setToggleCreateAnnouncement = useStore(
-    (state) => state.setToggleCreateAnnouncement
-  );
+  const [toggleSetting, setToggleSetting] = useState(false);
+  const userAuthenticated = useStore((state) => state.userAuthenticated);
+  const joined = useStore((state) => state.Joined);
+  const role = useStore((state)=>state.role)
+ 
   useEffect(() => {
     if (userData) {
       setuId(userData.id);
@@ -49,35 +45,36 @@ const GroupNav = () => {
       <nav className="flex flex-row md:gap-4 justify-between ">
         <h1 className="tmd:ext-xl font-semibold flex items-center md:gap-3">
           Category <ChevronsRight size={16} />{" "}
-          <span className="capitalize text-gray-700 dark:text-white/70 text:xs md:text-lg"> {category} </span>
+          <span className="capitalize text-gray-700 dark:text-white/70 text:xs md:text-lg">
+            {" "}
+            {category}{" "}
+          </span>
         </h1>
 
-        {isGrupAdmin &&userAuthenticated && (
+        {isGrupAdmin && userAuthenticated && (
           <div className="flex gap-4 justify-end items-center px-4 text-xs md:text-base relative">
-            <div className="flex items-center gap-2 px-4 text-sm py-2 bg-purple-600 rounded-full  text-white font-bold hover:bg-purple-700 cursor-pointer shadow-sm shadow-purple-500" onClick={()=>setToggleSetting(!toggleSetting)} >
+            <div
+              className="flex items-center gap-2 px-4 text-sm py-2 bg-purple-600 rounded-full  text-white font-bold hover:bg-purple-700 cursor-pointer shadow-sm shadow-purple-500"
+              onClick={() => setToggleSetting(!toggleSetting)}
+            >
               <Cog size={20} />
               <span>Setting</span>
             </div>
-            {
-              toggleSetting && (
-                <div className="absolute z-10 right-0 sm:right-2 md:right-auto  top-10 w-72 md:w-96 bg-white  dark:bg-gray-950 shadow-lg shadow-gray-500 dark:shadow-gray-700 dark:border-gray-600 dark:border p-4 rounded-xl">
-                 <SettingCard setToggleSetting={setToggleSetting} />
-                </div>
-              )
-            }
+            {toggleSetting && (
+              <div className="absolute z-10 right-0 sm:right-2 md:right-auto  top-10 w-72 md:w-96 bg-white  dark:bg-gray-950 shadow-lg shadow-gray-500 dark:shadow-gray-700 dark:border-gray-600 dark:border p-4 rounded-xl">
+                <SettingCard setToggleSetting={setToggleSetting} />
+              </div>
+            )}
           </div>
         )}
-        {!isGrupAdmin  &&joined && userAuthenticated && (
+        {role==="moderator" && joined && userAuthenticated && (
           <div className="flex  justify-end items-center px-4 text-xs md:text-base">
-            <button
-            
+            <Link
+              href={""}
               className="px-3 md:px-6 text-sx md:text-sm py-2 bg-purple-600 shadow-sm shadow-purple-500 rounded-full  text-white font-bold hover:bg-purple-700"
-              onClick={() =>
-                setToggleCreateAnnouncement(!toggleCreateAnnouncement)
-              }
             >
               New Announcement
-            </button>
+            </Link>
           </div>
         )}
       </nav>
