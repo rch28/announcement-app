504bab158854943ab55a77df769bd12436c3cead
submish <subodhmishra015@gmail.com>
2024-12-15 18:23:39 +0545

    feat: notification message according to type

--- src/components/layout/Notifications.jsx
+++ src/components/layout/Notifications.jsx
@@ -19,7 +19,17 @@ const Notifications = ({ notifications }) => {
       <h1 className="text-xl font-semibold">Notifications</h1>
       <ul>
         {notifications?.map((notification) => {
-          const date = parseDate(notification.created_at);
+          let date = new Date(notification.created_at);
+          const currentDate = new Date(); // Current date and time
+          
+          if (date.toISOString().split('T')[0] > currentDate.toISOString().split('T')[0]) {
+            // If the date is in the future, keep the date (YYYY-MM-DD)
+            date = date.toISOString().split('T')[0];
+          } else {
+            // If the date is today or earlier, keep the time (HH:MM:SS)
+            date = date.toTimeString().split(' ')[0];
+          }
+          
           return (
             <div
               key={notification.id}
@@ -32,7 +42,28 @@ const Notifications = ({ notifications }) => {
                 href={`/notifications/${notification.id}`}
                 className="flex items-start justify-between gap-4 font-medium"
               >
-                <h1>{notification.message}</h1>
+                <h1>
+                  {(() => {
+                    switch (notification.type) {
+                      case "group_join":
+                        return "New member joined the group!";
+                      case "group_leave":
+                        return "A member left the group.";
+                      case "announcement_create":
+                        return "New annonucement";
+                      case "announcement_update":
+                        return "Announcement updated";
+                      case "announcement_comment_create":
+                        return "New comment on the announcement";
+                      case "announcement_like":
+                          return "New like on the announcement";
+                      case "announcement_dislike":
+                        return "Dislike on the announcement";
+                      default:
+                        return "Notification received.";
+                    }
+                  })()}
+                </h1>
                 <p>
                   <DotsHorizontalIcon />
                 </p>
